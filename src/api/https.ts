7f8b5a002728086f62d913a022670199ffd34c0b
bsu0404 <bsu0404@naver.com>
2025-01-12 06:12:59 +0900

    feat: Create api instance, intercepotor

---
+++ src/api/https.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { getAccessToken } from "../store/auth";
+
+// application/json용
+export const api = axios.create({
+  baseURL: `http://${import.meta.env.VITE_API_URL}`,
+  timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+api.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = getAccessToken();
+    HeaderToken.set(token);
+
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const status = error.response?.status;
+
+    // 엑세스 토큰 만료
+    if (status === 401) {
+      console.log("status", status);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default class HeaderToken {
+  public static set = (accessToken: string | null): void => {
+    if (accessToken) {
+      api.defaults.headers.common.Authorization = `${accessToken}`;
+    } else {
+      delete api.defaults.headers.common.Authorization;
+    }
+  };
+}
