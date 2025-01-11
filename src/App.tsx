dec796c0c7dea044e29ae8426e0415bcecd73764
bsu0404 <bsu0404@naver.com>
2025-01-12 08:54:35 +0900

    feat: Add join path

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Rank from "./pages/rank/Rank";
 import Event from "./pages/event/Event";
 import Login from "./pages/login/Login";
+import Join from "./pages/join/Join";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/event" element={<Event />} />
         </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
