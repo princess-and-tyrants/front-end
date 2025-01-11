9c038ee80bb6934e0af6e6dfaa0159196f548a7d
bsu0404 <bsu0404@naver.com>
2025-01-12 08:53:21 +0900

    feat: Add join, checkId api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,3 +1,4 @@
+import { JoinReq } from "../pages/join/Join";
 import { LoginProps } from "../pages/login/Login";
 import { api } from "./https";
 
@@ -5,5 +6,14 @@ export const login = async (data: LoginProps) => {
   const response = await api.post<{ accessToken: string }>("/signin", {
     ...data,
   });
+  return response;
+};
+export const join = async (data: JoinReq) => {
+  const response = await api.post(`/signup`, { ...data });
+  return response;
+};
+
+export const checkId = async (id: string) => {
+  const response = await api.post(`/check/id?id=${id}`);
   return response;
 };
