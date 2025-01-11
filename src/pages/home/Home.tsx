c0a3ad8db796392356bd03cc81d526e8c222c8de
bsu0404 <bsu0404@naver.com>
2025-01-11 21:09:22 +0900

    feat: Apply zustand auth setting

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,46 @@
-import { Layout, PageLayout } from '../../components/layout/Layout';
+import { useState } from "react";
+import { Layout, PageLayout } from "../../components/layout/Layout";
+import useAuthStore from "../../store/auth";
 
 const Home = () => {
+  const { setToken, accessToken } = useAuthStore();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // login api
+    setToken(id + password);
+    // 홈으로 이동
+    window.location.href = "/";
+  };
   return (
     <PageLayout>
       <Layout>
-        <h2 className="f-heading2 f-mobile-title1">실시간 매장 현황</h2>
+        <h1>로그인</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>아이디</label>
+            <input
+              type="text"
+              id="id"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">로그인</button>
+        </form>
+        accessToken: {accessToken}
       </Layout>
     </PageLayout>
   );
