97d77a7e8886817876af27d4f26bca16366290d9
bsu0404 <bsu0404@naver.com>
2025-06-10 00:12:12 +0900

    feat: Creat Card page

---
+++ src/pages/card/Card.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layout/Layout";
+import "./card.scss";
+const Card = () => {
+  return (
+    <Layout>
+      <div className="title f-title2">친구들의 MBTiD를 확인해보세요!</div>
+    </Layout>
+  );
+};
+
+export default Card;
