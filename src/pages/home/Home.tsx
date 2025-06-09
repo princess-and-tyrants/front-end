f5ebfb825d68de586bf4f4212b53cbcde40796e6
bsu0404 <bsu0404@naver.com>
2025-06-10 02:49:38 +0900

    fix: move redirect logic into useEffect

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "@/store/auth";
 import { useMyProfileQuery } from "../../hook/profile/useMyInfoQuery";
@@ -6,22 +7,23 @@ import { useMyVoteListQuery } from "../../hook/vote/useVoteList";
 import MbtiPageTemplate from "@/components/templates/MbtiPageTemplate";
 import Loading from "@/components/common/loading/Loading";
 import "./home.scss";
-// import { Suspense } from "react";
-// import Loading from "react-loading";
 
 const HomeContent = () => {
   const { isLoggedIn } = useAuthStore();
-  const navitate = useNavigate();
+  const navigate = useNavigate();
 
   const { data: profileData, isLoading } = useMyProfileQuery(isLoggedIn);
   const { data: voteResult } = useMyVoteResultQuery(isLoggedIn);
   const { data: voteList } = useMyVoteListQuery(isLoggedIn);
 
-  if (!isLoggedIn) {
-    console.log("로그인이 필요합니다.");
-    navitate("/login");
-    return null;
-  }
+  useEffect(() => {
+    if (!isLoggedIn) {
+      console.log("로그인이 필요합니다.");
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
+
+  if (!isLoggedIn) return null;
 
   if (isLoading || !profileData) {
     return <Loading />;
