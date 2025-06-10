1427cbfa8c183952cfd383bf906aa4bb0bf5d804
bsu0404 <bsu0404@naver.com>
2025-06-11 02:19:41 +0900

    feat: enable custom color support for OutlineButton

--- src/components/button/OutlineButton.tsx
+++ src/components/button/OutlineButton.tsx
@@ -7,9 +7,17 @@ interface Props {
   type: "submit" | "reset" | "button";
   disabled?: boolean;
   onClick?: () => void;
+  color?: string;
 }
 
-const OutlineButton = ({ children, size, type, disabled, onClick }: Props) => {
+const OutlineButton = ({
+  children,
+  size,
+  type,
+  disabled,
+  onClick,
+  color = "#6C60C6",
+}: Props) => {
   return (
     <button
       data-size={size}
@@ -17,6 +25,7 @@ const OutlineButton = ({ children, size, type, disabled, onClick }: Props) => {
       onClick={onClick}
       type={type}
       className={`ouline-button ${disabled ? "disabled" : ""} ${size == "small" ? "f-caption" : "f-body1"}`}
+      style={{ color: color }}
     >
       {children}
     </button>
