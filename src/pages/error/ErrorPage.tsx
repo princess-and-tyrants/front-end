54976b6e3a7e8916bfbeac43e6836d520f67c7ff
bsu0404 <bsu0404@naver.com>
2025-06-11 02:20:39 +0900

    feat: add error page component

---
+++ src/pages/error/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import { Layout } from "@/components/layout/Layout";
+import "./ErrorPage.scss"; //
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <div className="error-page">
+        <h1 className="error-message f-title1">문제가 발생했습니다.</h1>
+        <p className="error-description f-body2">
+          죄송합니다. 잠시 후 다시 시도해주세요.
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default ErrorPage;
